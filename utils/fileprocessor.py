c5ae063b7c460eb11a22ed0ede509dac24b8be8a
ehom <ehom@users.noreply.github.com>
2023-01-19 18:57:54 -0800

    commented out debug print statements. Will remove them at some point.

--- utils/fileprocessor.py
+++ utils/fileprocessor.py
@@ -1,8 +1,9 @@
 from .str_lint import lint
 from pprint import PrettyPrinter
+import logging
 
 pp = PrettyPrinter(
-    indent=4,
+    indent=2,
     width=100,
     compact=True
 )
@@ -18,18 +19,18 @@ class FileProcessor:
     def execute(self) -> dict:
         try:
             self.content = self.reader.read()
-            pp.pprint(self.content)
+            # pp.pprint(self.content)
         except FileNotFoundError:
-            print("Error: File Not Found: {}".format(self.reader.filename))
+            print("Error: File Not Found: {0}".format(self.reader.filename))
             return {}
 
-        # lookup table that maps a message to its findings
+        # lookup-table that maps a message to its findings
         findings = {}
         for message_id, message in self.content.items():
             if message is None:
                 continue
-            print("processing....message_id:", message_id)
-            print("processing....message:", message)
+
+            logging.info("Processing...\"{0}\": \"{1}\"".format(message_id, message))
 
             if type(message) is dict and message['message'] is not None:
                 message = message['message']
@@ -43,17 +44,13 @@ class FileProcessor:
                 "linted": []
             }
 
-            print("'{0}': \"{1}\"\n".format(message_id, message))
-
             found_something = lint(message)
             if len(found_something):
-                print("found_something:", found_something)
-                print("'{0}': >>> \"{1}\"\n".format(message_id, message))
-
+                print(">>> '{0}': \"{1}\"".format(message_id, message))
                 for something in found_something:
                     findings[message_id]["linted"].append(something['desc'])
-                # print(self.reader.filename)
-                # pp.pprint(findings)
+                    print(">>> {0}".format(something['desc']))
+                print('~' * 10)
                 self.writer.write(findings)
         return findings
 
