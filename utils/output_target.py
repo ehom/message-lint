4207a587333b3cc09548a5c0ed5721fc88312826
ehom <ehom@users.noreply.github.com>
2023-02-05 16:39:51 -0800

    refactor derive_output_target, make it generic

---
+++ utils/output_target.py
@@ -0,0 +1,19 @@
+import os
+import pathlib
+
+
+def derive_output_target(filename: str, target_path: str) -> dict:
+    def derive_folder_path_and_filename(fname: str) -> dict:
+        abspath = os.path.abspath(fname)
+        p = pathlib.Path(abspath)
+        return dict(folder_path=p.parents[0], filename=p.name)
+
+    info = derive_folder_path_and_filename(filename)
+
+    # if a custom output folder was provided at the command line
+    if target_path:
+        folder_path = os.path.abspath(target_path)
+    else:
+        folder_path = info['folder_path']
+
+    return dict(folder_path=folder_path, file_path=info['filename'])
